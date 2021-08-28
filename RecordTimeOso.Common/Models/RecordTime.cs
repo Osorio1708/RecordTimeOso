8678f411aa2c46348ffdf547b5dfb4bf9edd099a
Santiago Osorio <saostero@gmail.com>
2021-08-28 14:15:48 -0500

    Se definen modelos y estructura de datos

---
+++ RecordTimeOso.Common/Models/RecordTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecordTimeOso.Common.Models
+{
+    public class RecordTime
+    {
+        public int IdEmployee { get; set; }
+        public DateTime TimeRecorded { get; set; }
+        public Byte RecordTipe { get; set; }
+        public bool CoConsolidated { get; set; }
+    }
+}
---
+++ RecordTimeOso.Functions/Entities/RecordTimeEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace RecordTimeOso.Functions.Entities
+{
+    public class RecordTimeEntity : TableEntity
+    {
+        public int IdEmployee { get; set; }
+        public DateTime TimeRecorded { get; set; }
+        public Byte RecordTipe { get; set; }
+        public bool CoConsolidated { get; set; }
+    }
+}
